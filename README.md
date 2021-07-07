3f3ee28a50177672615e5a747afe17e9f6582771
John Chen <john@johnchen.co>
2021-07-07 15:59:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web Chat SDK Samples
+
+This repo holds example apps showing various implementations of Kustomer's Web Chat SDK. For example, using chat behind authentication.
+
+## Setup
+
+1. Clone this repository
+2. Each directory has its own sample app and `README.md`
+
+## General Documentation
+
+[https://developer.kustomer.com/chat-sdk/v2.0-Web/docs/requirements](https://developer.kustomer.com/chat-sdk/v2.0-Web/docs/requirements)
---
+++ login/server.js
@@ -0,0 +1,53 @@
+// Before using this sample app, you must have generated a secret key by completing Step 1 and 2 of this guide
+// https://developer.kustomer.com/chat-sdk/v2.0-Web/docs/authenticate-chat-with-jwt-token
+// and replaced SECRET_KEY in this file
+
+const path = require('path');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+const MOCK_DATABASE = {
+  users: [
+    { id: 'uuid1', name: 'Amy', email: 'amy@amy.com' },
+    { id: 'uuid2', name: 'Barry', email: 'barry@barry.com' },
+  ],
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.post('/login', (req, res) => {
+  // let's say just for this example that Amy is logging in
+  const userFromDB = MOCK_DATABASE.users[0];
+
+  var kustomerLoginJwtToken = jwt.sign(
+    {
+      externalId: userFromDB.id,
+      email: userFromDB.email,
+      iat: Math.floor(Date.now() / 1000),
+    },
+    // replace SECRET_KEY with your own by following Steps 1 and 2 here
+    // https://developer.kustomer.com/chat-sdk/v2.0-Web/docs/authenticate-chat-with-jwt-token
+    'SECRET_KEY',
+    {
+      header: {
+        alg: 'HS256',
+        typ: 'JWT',
+      },
+    }
+  );
+
+  res.send({
+    id: userFromDB.id,
+    name: userFromDB.name,
+    email: userFromDB.email,
+    kustomerLoginJwtToken: kustomerLoginJwtToken,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Example login app listening at http://localhost:3000`);
+});
